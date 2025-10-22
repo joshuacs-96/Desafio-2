73e09a7e2348d2f22b61dcf0c8e02d23419f8f00
Joshua Castro <joshua.csolarte@udea.edu.co>
2025-10-22 00:16:56 -0500

    primera clase

---
+++ Desafio2/udeatunes.cpp
@@ -0,0 +1,3 @@
+#include "udeatunes.h"
+
+UdeATunes::UdeATunes() {}
---
+++ Desafio2/udeatunes.h
@@ -0,0 +1,9 @@
+#ifndef UDEATUNES_H
+#define UDEATUNES_H
+
+class UdeATunes { // << Se eliminó la herencia
+public:
+    UdeATunes();
+};
+
+#endif // UDEATUNES_H
